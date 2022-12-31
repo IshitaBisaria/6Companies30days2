5db2f930800e015dcf2d90c81b38f4b66504790c
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2022-12-31 12:20:30 +0530

    Update 1. Evaluate Reverse Polish Notation.cpp

--- Day 1 to 5/1. Evaluate Reverse Polish Notation.cpp
+++ Day 1 to 5/1. Evaluate Reverse Polish Notation.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(int i = 0; i< tokens.size(); i++) {
+            if(tokens[i].size() > 1 || isdigit(tokens[i][0]))
+                st.push(stoi(tokens[i]));
+            else {
+                int val1 = st.top();
+                st.pop();
+                int val2 = st.top();
+                st.pop();
+                switch(tokens[i][0]) {
+                    case '+' : st.push(val1 + val2);
+                                break;
+                    case '-' : st.push(val2 - val1);
+                                break;
+                    case '*' : st.push(val2 * val1);
+                                break;
+                    case '/' : st.push(val2 / val1);
+                                break;
+                }
+            }
+        }
+        return st.top();
+    }
+};
